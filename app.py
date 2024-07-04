afb03faef5defb2227b2260ccbcc61c0de044960
SecretaryOats <audreymlp34@gmail.com>
2024-07-04 22:46:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,56 @@
-from flask import Flask, request, render_template, redirect, url_for
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
 from PIL import Image
-import colorsys
 import os
+import colorsys
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
-def get_triadic_colors(r, g, b):
-    h, l, s = colorsys.rgb_to_hls(r / 255.0, g / 255.0, b / 255.0)
-    triadic_colors = []
-    for i in range(3):
-        triadic_h = (h + i * 1/3) % 1.0
-        triadic_r, triadic_g, triadic_b = colorsys.hls_to_rgb(triadic_h, l, s)
-        triadic_colors.append((int(triadic_r * 255), int(triadic_g * 255), int(triadic_b * 255)))
-    return triadic_colors
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+def get_palette(image_path, num_colors=5):
+    image = Image.open(image_path)
+    image = image.convert('RGB')
+    colors = image.getcolors(image.size[0] * image.size[1])
+    colors.sort(key=lambda x: x[0], reverse=True)
+    palette = [color[1] for color in colors[:num_colors]]
+    return palette
+
+def classify_season(color):
+    r, g, b = color
+    if r > g and g > b:
+        return 'Autumn'
+    elif g > r and r > b:
+        return 'Spring'
+    elif b > r and r > g:
+        return 'Winter'
+    else:
+        return 'Summer'
+
+def classify_warm_cool(color):
+    r, g, b = color
+    return 'Warm' if (r + g) > b else 'Cool'
+
+def classify_saturation(color):
+    r, g, b = color
+    max_color = max(r, g, b)
+    min_color = min(r, g, b)
+    return 'Saturated' if (max_color - min_color) > 100 else 'Desaturated'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file:
-            image = Image.open(file.stream)
-            # Assuming the skin color is the most frequent color in the image for simplicity
-            colors = image.getcolors(image.size[0] * image.size[1])
-            most_frequent_color = max(colors, key=lambda t: t[0])[1]
-            triadic_colors = get_triadic_colors(*most_frequent_color)
-            return render_template('index.html', triadic_colors=triadic_colors)
+        files = request.files.getlist('image')
+        palettes = []
+        for file in files:
+            if file:
+                image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                file.save(image_path)
+                palette = get_palette(image_path)
+                palettes.append(palette)
+        return render_template('index.html', palettes=palettes)
     return render_template('index.html')
 
 if __name__ == '__main__':
